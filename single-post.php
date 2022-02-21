7c5a0e8677157c1acabe38994e89a8e0a34837da
Damien Dufresne <e1995508@cmaisonneuve.qc.ca>
2022-02-21 15:07:12 -0500

    exercice 2 1e commit

---
+++ single-post.php
@@ -0,0 +1,16 @@
+<?php get_header() ?>
+<main class="site__main">
+<h1>---- single-post.php ------</h1>
+<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
+     <article class="cours">
+          <?php the_post_thumbnail("medium"); ?>
+          <h1 class="courstitre">
+               <?php the_title(); ?>
+          </h1>
+          <?php the_content(); ?>
+
+     </article>
+<?php endwhile;?>
+<?php endif;?>
+</main>
+<?php get_footer() ?>
