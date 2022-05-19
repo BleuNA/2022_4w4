b57ab3a7428951db59720df6f5761704d31a12d4
Damien Dufresne <e1995508@cmaisonneuve.qc.ca>
2022-05-19 14:30:45 -0400

    ef-DD Enlevage de ligne (suite)

--- template-atelier.php
+++ template-atelier.php
@@ -9,7 +9,6 @@
 <?php get_header(); ?>
 <main class="site__main">
     <article class="atelier">
-    <!-- <h1>---- page.php ------</h1> -->
     <?php if (have_posts()) : the_post(); ?>
         <h1><?php the_title() ?></h1>
         <section class="atelier__bloc1">
